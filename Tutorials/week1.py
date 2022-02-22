b76b7f8eed455a7ebd7519ec4a5430835c506b00
LukeCitrine <curiousbox27@gmail.com>
2022-02-22 13:48:30 +0000

    added folders for design pictures, changed some files.

--- Tutorials/week1.py
+++ Tutorials/week1.py
@@ -1,2 +1,18 @@
-for x in range(10):
-    print("hello world!" + str(x))
+#Printing strings together
+from tkinter.font import names
+
+
+str1 = "Hello"
+str2 = "World"
+message = (f"{str1.lower()}, {str2.upper()}!")
+print(message)
+
+#Printing Lists/changing list element
+nameslist = ["Laura","Luke","Shane"]
+nameslist[1] = "Cameron"
+nameslist.reverse() #reverse numbers in list.
+print(nameslist)
+
+#Looping in a list
+numbers = ["Zero", "One", "Two"]
+print(numbers)
